1cd49e41a55863df95992d80cc2433a477a46d60
TheGreatKapybara <oliver.shustar@gmail.com>
2023-03-05 20:21:27 +0500

    beta version

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import openai
 from aiogram import Bot, types, Dispatcher, executor
 
+
 bot = Bot("6179571731:AAGLvplhbJdi1L5NGcewOM-O4Z2M5fOxjjU")
 dp = Dispatcher(bot)
 
-openai.api_key = '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'
+openai.api_key = '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'
 
-@dp.message_handler(commands="start")
+pin_counter = 0
+@dp.message_handler(commands=["start", "help"])
 async def hello_message(message: types.Message):
-    await bot.send_photo(chat_id=message.chat.id, photo="https://www.rollingstone.com/wp-content/uploads/2018/06/rs-219361-SW_ep4_090___.jpg?resize=300")
-    await message.answer(text=f"Привет! Я chat GPT, но теперь я научился использовать телеграмм для общения с пользователями!\n"
+    await bot.send_photo(chat_id=message.chat.id, photo=open("images/welcome_img.png", "rb"))
+    to_pin = await message.answer(text=f"Привет! Я chat GPT, но теперь я научился использовать телеграмм для общения с пользователями!\n"
                               "Можешь писать мне на любом удобном для тебя языке. Чтобы начать общаться просто напиши своё сообщение мне в чат.")
+    global pin_counter
+    if pin_counter < 1:
+        await bot.pin_chat_message(chat_id=message.from_user.id, message_id=to_pin.message_id)
+        pin_counter += 1
     await message.delete()
 
 
 @dp.message_handler()
 async def message_sender(message: types.Message):
+    bot_thinking_message = await message.answer(text="Я думаю🤔")
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
         temperature=0.9,
-        max_tokens=1000,
-        top_p=1.0,
+        max_tokens=3000,
+        top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
-        stop=[" You:"]
     )
-
-    await message.answer(response['choices'][0]['text'])
+    await bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+    await bot.delete_message(message_id=bot_thinking_message.message_id, chat_id=message.from_user.id)
 
 executor.start_polling(dp, skip_updates=True)
+
+
